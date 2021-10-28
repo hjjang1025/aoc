b41e53adfc80904a42c809302d6fb7eaf7a7ea9f
hjjang1025 <hyojin.jang@greenlabs.co.kr>
2021-10-28 18:45:38 +0900

    refactoring & threading macro

--- src/aoc_2018_1.clj
+++ src/aoc_2018_1.clj
@@ -1,15 +1,17 @@
 (ns aoc-2018-1
   (:require [clojure.string :as str]))
 
-;;[Part 1]
+;;===========[Part 1]===========
+
 ;;file read
-(def input_list
+(def input-list
   (str/split-lines (slurp "src/input_2018_1.txt")))
 
-;;string -> int
-(defn toInt [str]
-  (if (nil? str) 0 (Integer/parseInt str) ))
-
 ;;sum of list
-(reduce + (map toInt input_list))
+(reduce + (map #(Integer/parseInt %) input-list))
 
+;;threading macro
+(->> (slurp "src/input_2018_1.txt")
+     str/split-lines
+     (map #(Integer/parseInt %))
+     (reduce +))
