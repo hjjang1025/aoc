087d8dbdd708480c4f64a999169bdb6ff9f89e3a
hjjang1025 <hyojin.jang@greenlabs.co.kr>
2021-10-31 20:04:03 +0900

    solved day2/part2

--- src/aoc_2018_1.clj
+++ src/aoc_2018_1.clj
@@ -25,16 +25,17 @@
 
 ;; 2. 변경치의 누적값을 계산
 ;; 3. 누적값 중 처음으로 중복되는 값을 구하기
-(loop [rest-cycle int-cycle frequency-set #{} last-frequency 0]
+(loop [rest-cycle int-cycle
+       frequency-set #{}
+       last-frequency 0]
   (let [change (first rest-cycle)
-        rest-cycle (rest rest-cycle)
-        ]
+        rest-cycle (rest rest-cycle)]
       (if (frequency-set last-frequency)
-        (do last-frequency)
+        last-frequency
         (recur rest-cycle
                (conj frequency-set last-frequency)
-               (+ last-frequency change))
-        )))
+               (+ last-frequency change)))))
+
 
 
 
--- src/aoc_2018_2.clj
+++ src/aoc_2018_2.clj
@@ -1,5 +1,6 @@
 (ns aoc-2018-2
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.math.combinatorics :as combo]))
 
 ;===========[Part 1]===========
 
@@ -8,11 +9,49 @@
   (str/split-lines (slurp "src/input_2018_2.txt")))
 
 (defn count-duplicate [times str]
-  (count (filter (fn [[k v]] (= v times)) (frequencies str)) )
-  )
+  (count (filter (fn [[_ v]] (= v times)) (frequencies str))))
+
 
 (*
   (count (filter pos-int? (map (partial count-duplicate 2) input-list)))
-  (count (filter pos-int? (map (partial count-duplicate 3) input-list)))
-  )
+  (count (filter pos-int? (map (partial count-duplicate 3) input-list))))
+
+
+
+
+;===========[Part 2]===========
+; list to vector
+(def ids
+  (->> (slurp "src/input_2018_2.txt")
+       str/split-lines
+       vec))
+
+;(common-between "abcde" "abcee") => abce
+;(common-between "abcdefff" "abceeggg") => nil
+(defn common-between [str-a str-b]
+  (let [common-str (->> (map vector str-a str-b)
+                        (filter (fn [[x y]] (= x y)))
+                        (map first)
+                        str/join)]
+    (when (= (count common-str) (- (count str-a) 1))
+      common-str)))
+
+; 연산할 조합 리스트
+; ["a" "b"] ["a" "b"] => (("a" "a") ("a" "b") ("b" "a") ("b" "b"))
+(def ids-cartesian-product (combo/cartesian-product ids ids))
+
+(comment
+  (loop [[str-a str-b] (first ids-cartesian-product)
+         rest-cartesian-product (rest ids-cartesian-product)]
+    (if (common-between str-a str-b)
+      (common-between str-a str-b)
+      (recur (first rest-cartesian-product)
+             (rest rest-cartesian-product)))))
+
+
+
+
+
+
+
 
