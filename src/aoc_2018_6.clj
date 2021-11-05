0320af6c54345db087101103babb7062d41cd3f4
hjjang1025 <hyojin.jang@greenlabs.co.kr>
2021-11-05 18:57:46 +0900

    solved day6/part1

--- src/aoc_2018_6.clj
+++ src/aoc_2018_6.clj
@@ -7,22 +7,15 @@
         (re-matches #"([0-9]+), ([0-9]+)" str)]
     [(Integer/parseInt x) (Integer/parseInt y)]))
 
-(defn manhattan-distance [[a b] [c d]]
-  (+ (Math/abs (- a c))
-     (Math/abs (- b d))))
+(def chronal-coordinates
+  (->> (slurp "resources/input_2018_6.txt")
+       str/split-lines
+       (map parse-coordinate)))
 
-(manhattan-distance [1 4] [5 8])
-
-;parse
-#_(def chronal-coordinates
-    (->> (slurp "resources/input_2018_6.txt")
-         str/split-lines
-         (map parse-coordinate)))
-
-(def chronal-coordinates [[1 1] [1 6] [8 3] [3 4] [5 5] [8 9]])
-
-;processing 1 generate area
-(defn area [coordinates]
+(defn area
+  "Processing 1
+  area : 좌표 최대값, 최소값으로 추출한 좌표 sequence"
+  [coordinates]
   (let [xs (map first coordinates)
         ys (map second coordinates)
         min-x (apply min xs)
@@ -33,38 +26,58 @@
           ys (range min-y (+ max-y 1))]
       [xs ys])))
 
-;processing 2
-;area -> manhattan-distance
 
-;processing 3
-; index-of-min-distance
-; (0 5 9 5 8 15) => 0
-; (1 1 9 5 8 15) => nil ;두 좌표 이상 거리가 같다
-; (11 6 6 6 3 4) => 4
+(defn manhattan-distance [[a b] [c d]]
+  (+ (Math/abs (- a c))
+     (Math/abs (- b d))))
 
-(defn index-of-min-distance [distance-list]
+(defn index-of-min-distance
+  "
+  Processing 2
+  index-of-min-distance
+  (0 5 9 5 8 15) => 0
+  (1 1 9 5 8 15) => nil ;맨하튼 거리가 같은 좌표가 2개 이상
+  (11 6 6 6 3 4) => 4
+  "
+  [distance-list]
   (let [min-distance (apply min distance-list)]
     (when (= 1 (get (frequencies distance-list) min-distance))
       (.indexOf distance-list min-distance))))
 
-(index-of-min-distance '(0 5 9 5 8 15))
-(index-of-min-distance '(1 1 9 5 8 15))
-(index-of-min-distance '(11 6 6 6 3 4))
+(defn index-of-closest-coordinate [coordinates coordinate]
+  (->> coordinates
+       (map (partial manhattan-distance coordinate))
+       index-of-min-distance))
 
+(defn area-boundary
+  "area의 가장자리에 있는 좌표 sequence"
+  [coordinates]
+  (let [xs (map first coordinates)
+        ys (map second coordinates)
+        min-x (apply min xs)
+        min-y (apply min ys)
+        max-x (apply max xs)
+        max-y (apply max ys)]
+    (->> (for [xs (range min-x (+ max-x 1))
+               ys (range min-y (+ max-y 1))]
+           (when (or (= xs min-x)
+                     (= ys min-y)
+                     (= xs max-x)
+                     (= ys max-y)) [xs ys]))
+         (keep identity))))
 
-(defn manhattan-distance-list [coordinate]
+(def infinite-coordinates
+  "무한 확장하는 좌표 set"
   (->> chronal-coordinates
-       (map (partial manhattan-distance coordinate))))
-
-
-(manhattan-distance-list [3 3])
-
+       area-boundary
+       (map (partial index-of-closest-coordinate chronal-coordinates))
+       (keep identity) ;nil 제거
+       set))
 
 (comment
-  (->> chronal-coordinates
-       area
-       (map manhattan-distance-list)
-       (map index-of-min-distance)
-       frequencies
-       (sort-by val)))
-
+  (->> chronal-coordinates ;Parse
+       area ;Processing 1
+       (map (partial index-of-closest-coordinate chronal-coordinates)) ;Processing2
+       frequencies ;Aggregation
+       (filter #(->> % key infinite-coordinates not)) ;[index-of-coordinate 면적]
+       (apply max-key second))) ;면적 기준으로 최고값 추출
