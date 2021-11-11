574ea5f4580236ab1340f62d50c9bd14a5b88e16
hjjang1025 <hyojin.jang@greenlabs.co.kr>
2021-11-11 16:44:20 +0900

    refactoring and print함수 추가

--- src/aoc_2018_4.clj
+++ src/aoc_2018_4.clj
@@ -2,31 +2,29 @@
   (:require [clojure.string :as str]))
 
 ;===========[Part 1]===========
-;Usage
-;(match "[1518-11-01 00:00] Guard #10 begins shift")
-(defn match [str]
-  (let [[_ year month day hour minute action]
+
+(defn parse-guard-number [str]
+  (when-let [guard (->> (re-matcher #"\d+" str)
+                        re-find)]
+    (Integer/parseInt guard)))
+
+
+
+(defn parse-record [str]
+  (let [[_ year month day hour minute guard]
         (re-matches #"\[([0-9]+)-([0-9]+)-([0-9]+) ([0-9]+):([0-9]+)\] (.*)" str)]
     {:year (Integer/parseInt year)
      :month (Integer/parseInt month)
      :day (Integer/parseInt day)
      :hour (Integer/parseInt hour)
      :minute (Integer/parseInt minute)
-     :action action}))
+     :guard (parse-guard-number guard)}))
 
-(def input-list
+(def records
   (->> (slurp "resources/input_2018_4.txt")
        str/split-lines
        sort
-       (map match)))
-
-
-;Usage
-;(match-guard {:year 1518, :month 10, :day 29, :hour 23, :minute 52, :action "Guard #1601 begins shift"})
-(defn match-guard [{:keys [action]}]
-  (->> (re-matcher #"\d+" action)
-       re-find))
-
+       (map parse-record)))
 
 ;1.-----------------------
 ;record to logs
@@ -34,18 +32,19 @@
 ; {10 [2 3 5 33]}
 ; {587 [10 16 38 53]})
 
-;Refactoring⭐ [logs last-guard] -> {:keys [logs last-guard]}
+;⭐ [logs last-guard] -> {:keys [logs last-guard]}
 
-(defn generate-logs [{:keys [logs last-guard]} record]
-  (if-let [new-guard (match-guard record)]
-    {:logs (conj logs {new-guard []})
+(defn generate-guard-minute-logs [{:keys [logs last-guard]} record]
+  (if-let [new-guard (record :guard)] ;새 guard 등장하면
+    {:logs (conj logs {new-guard []}) ;빈 벡터로 교체
      :last-guard new-guard}
     {:logs (conj (pop logs)
-                 {last-guard (conj (first (vals (last logs))) (:minute record))})
+                 {last-guard (conj (flatten (vals (last logs)))
+                                   (:minute record))})
      :last-guard last-guard}))
 
 ;2.-----------------------------------
-;log-list to log-map (같은 guard 병합) ⭐apply 함수로 사용(line:92)
+;log-list to log-map (같은 guard 병합) ⭐apply 함수로 사용
 
 ;({10  [10 16 38 53]}
 ; {10 [2 3 5 33]}
@@ -58,37 +57,53 @@
 ;minute -> range list
 ;[10 16 38 53 2 3 5 33]  =>  (10, 11, 12, 13, .., 15, 38, 39, ... 52, 2, 5, 6, 7, ... 32)
 
-;Usage
-;(log-to-range [10 16 38 53 2 3 5 33])
-(defn log-to-range [vec]
-  (->> vec
+
+(defn all-sleep-minutes
+  "minute -> range list
+  (all-minutes-sleep [10 16 38 53 2 3 5 33])
+  => [10 16 38 53 2 3 5 33]  =>  (10, 11, 12, 13, .., 15, 38, 39, ... 52, 2, 5, 6, 7, ... 32)
+  "
+  [repeat-sleep-wake]
+  (->> repeat-sleep-wake
        (partition 2)
-       (map (fn [[sleep wake]] (range sleep wake))) ;Refactoring⭐ fn 추가해서 range 적용
+       (map (fn [[sleep wake]] (range sleep wake))) ;⭐ fn 추가해서 range 적용
        (reduce concat)))
 
 ;4-------------------------------
-;get duration (count list) and frequencies
-(defn range-to-info "get duration and frequencies" [guard list]
-  {:guard guard
-   :duration (count list)
-   :frequencies (sort-by val (frequencies list))})
+(defn format-guard-sleep-log
+  "get duration and frequencies"
+  [guard sleep-minutes]
+  {:guard       guard
+   :duration    (count sleep-minutes)
+   :minute-frequencies (sort-by val (frequencies sleep-minutes))})
 
+(defn generate-guard-sleep-frequencies-log [records]
+  (->> records
+       (reduce generate-guard-minute-logs {:logs [] :last-guard 0}) ;1
+       :logs
+       (apply (partial merge-with into)) ;2 ⭐ apply 사용 (같은 guard 병합)
+       (map (fn [[guard repeat-sleep-wake]]
+              (format-guard-sleep-log guard
+                                      (all-sleep-minutes repeat-sleep-wake))))))  ;3, 4)
+
+(defn print-answer [{:keys [guard minute-frequencies]}]
+  (* guard (-> minute-frequencies
+               last
+               first)))
 
 (comment
-  (->> input-list
-       (reduce generate-logs {:logs [] :last-guard 0}) ;1
-       :logs
-       (apply (partial merge-with into)) ;2 Refactoring⭐ 불필요한 함수 삭제 후 apply 사용
-       (map (fn [[k v]] (range-to-info k (log-to-range v))))  ;3, 4
-       (apply max-key :duration))) ;Refactoring⭐ 최대값 구하기->sort 대신 max-key로 변경
+  (->> records
+       generate-guard-sleep-frequencies-log
+       (apply max-key :duration)
+       print-answer))
+   ;⭐ 최대값 구하기->sort 대신 max-key로 변경
 
 
 ;===========[Part 2]===========
+
 (comment
-  (->> input-list
-       (reduce generate-logs {:logs [] :last-guard 0})
-       :logs
-       (apply (partial merge-with into))
-       (map (fn [[k v]] (range-to-info k (log-to-range v)))) ;--여기까지 Part 1과 동일합니다--
-       (filter #(-> % :frequencies seq)) ; NullPointException을 막기 위해 한번도 잠들지 않은 guard를 filter로 제거
-       (apply max-key #(-> % :frequencies last second)))) ; 가장 큰 frequency 기준으로 최대값 추출
+  (->> records
+       generate-guard-sleep-frequencies-log
+       (filter #(-> % :minute-frequencies seq)) ; NullPointException을 막기 위해 한번도 잠들지 않은 guard를 filter로 제거
+       (apply max-key #(-> % :minute-frequencies last second)) ; 가장 큰 frequency 기준으로 최대값 추출
+       print-answer))
