fc59fd84226d104102ee0c9f286632ca667065d9
hjjang1025 <hyojin.jang@greenlabs.co.kr>
2021-11-11 16:09:10 +0900

    refactoring : naming, apply 제거

--- src/aoc_2018_2.clj
+++ src/aoc_2018_2.clj
@@ -5,18 +5,20 @@
 ;===========[Part 1]===========
 
 ;file read
-(def input-list
+(def box-ids
   (str/split-lines (slurp "resources/input_2018_2.txt")))
 
+
 ;[⭐1]️ frequencies 를 count-times-frequencies 밖에서 계산
-(defn count-times-frequencies [times frequencies]
-  (count
-    (filter (fn [[_ v]] (= v times))
-            frequencies)))
+(defn count-times-frequencies [times alphabet-frequencies]
+  (->> alphabet-frequencies
+       (filter (fn [[_ v]] (= v times)))
+       count))
+
 
 ;[⭐2] 문제의 정답을 계산할 때 반복되는 부분을 함수로 분리
-(defn count-duplicate [times seq]
-  (->> seq
+(defn count-duplicate-alphabet [times box-id]
+  (->> box-id
        (map frequencies) ;[⭐1]
        (map (partial count-times-frequencies times))
        (filter pos-int?)
@@ -24,10 +26,10 @@
 
 ;[⭐3] ->> 로 정리
 (comment
-  (* (->> input-list (count-duplicate 2))
-     (->> input-list (count-duplicate 3))))
+  (* (->> box-ids (count-duplicate-alphabet 2))
+     (->> box-ids (count-duplicate-alphabet 3))))
 
-
+;parse / process / aggregate / print
 
 ;===========[Part 2]===========
 ; list to vector
@@ -38,30 +40,24 @@
 
 ;(common-between "abcde" "abcee") => abce
 ;(common-between "abcdefff" "abceeggg") => nil
-(defn common-between [str-a str-b]
+; 함수 분리하기
+(defn common-letters-between [[str-a str-b]]
   (let [common-str (->> (map vector str-a str-b) ; [(\a \a) (\b \b) (\c \c) (\d \e) (\e \e)]
                         (filter (fn [[x y]] (= x y))) ; [(\a \a) (\b \b) (\c \c) (\e \e)]
                         (map first) ; [\a \b \c \e]
                         str/join)]  ; abce
-    (when (= (count common-str) (- (count str-a) 1))
+    (when (= (count common-str)
+             (- (count str-a) 1))
       common-str)))
 
+;;;;
 ; 연산할 조합 리스트
 ; ["a" "b" "c"] => (("a" "b") ("a" "c") ("b" "c"))
-;Refactoring⭐️ combinations 사용
+;⭐️ combinations 사용
 (def ids-cartesian-product (combo/combinations ids 2))
-
-;🔥loop
-(comment
-  (loop [[str-a str-b] (first ids-cartesian-product)
-         rest-cartesian-product (rest ids-cartesian-product)]
-    (if (common-between str-a str-b)
-      (common-between str-a str-b)
-      (recur (first rest-cartesian-product)
-             (rest rest-cartesian-product)))))
 
 ;⭐️Reduce
 (comment
   (->> ids-cartesian-product
-       (keep #(apply common-between %))))
+       (keep #(common-letters-between %))))
 
