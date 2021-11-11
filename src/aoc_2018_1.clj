12b94d8ca26e930b47001e0827bbc1a7c04d1de3
hjjang1025 <hyojin.jang@greenlabs.co.kr>
2021-11-11 13:38:22 +0900

    refactoring : 변수명, reduction, reduced

--- src/aoc_2018_1.clj
+++ src/aoc_2018_1.clj
@@ -11,21 +11,23 @@
 (reduce + (map #(Integer/parseInt %) input-list))
 
 ;threading macro
-(->> (slurp "resources/input_2018_1.txt")
+(->> (slurp "resources/input_2018_1.txt") ;[PARSE]
      str/split-lines
      (map #(Integer/parseInt %))
-     (reduce +))
+     (reduce +)) ;[AGGREGATION]
 
 
 ;===========[Part 2]===========
 (def int-list (map #(Integer/parseInt %) input-list))
 
 ;; 1. 변경치들을 반복적으로 읽어들임
-(def int-cycle (cycle int-list))
+(def frequency-adjustments (->> input-list
+                                (map #(Integer/parseInt %))
+                                cycle))
 
 ;; 2. 변경치의 누적값을 계산
 ;; 3. 누적값 중 처음으로 중복되는 값을 구하기
-(loop [rest-cycle int-cycle
+(loop [rest-cycle frequency-adjustments
        frequency-set #{}
        last-frequency 0]
   (let [change (first rest-cycle)
@@ -37,6 +39,18 @@
                (+ last-frequency change)))))
 
 
+;[⭐️reductions : reduce 중간 결과 sequence]
 
+(def frequency-history (reductions + frequency-adjustments))
 
+(defn first-duplicate-frequency
+  [history]
+  (reduce (fn [frequency-set frequency]
+            (if (frequency-set frequency)
+              (reduced frequency)
+              (conj frequency-set frequency)))
+          #{} history))
 
+(comment
+  (->> frequency-history
+       first-duplicate-frequency))
